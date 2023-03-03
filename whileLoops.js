c3e1a5598ab225a7043d2facd970fd87b443949e
Somash Manandhar <98387934+somash07@users.noreply.github.com>
2023-03-03 20:35:07 +0545

    while and do-while

---
+++ whileLoops.js
@@ -0,0 +1,18 @@
+//while loop
+let n= prompt("enter the value of n");
+n=Number.parseInt(n);
+let i=0;
+while(i<n){
+    console.log(i);
+    i++;
+}
+//dowhile loop->executes atleast once
+//first excecutes and condition check
+let m= prompt("enter the value of n");
+m=Number.parseInt(n);
+let j=0;
+do{
+    console.log(i);
+    i++;
+}while(j<m)
+
