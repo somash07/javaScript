843dc1691023cd839263fdb96a723bcc900bcee6
Somash Manandhar <98387934+somash07@users.noreply.github.com>
2023-03-03 20:45:20 +0545

    syntax: function,arrow function

---
+++ function.js
@@ -0,0 +1,20 @@
+/*function function_name(arg){
+    codes;
+}*/
+
+
+/*
+arrow function
+const function_name=(arg)=>{
+    codes;
+}
+*/
+const sum=(a,b)=>{
+    return a+b;
+}
+
+const hello=()=>{
+    console.log("function with no return value");
+}
+hello();//invoking
+console.log(sum(2,3));
